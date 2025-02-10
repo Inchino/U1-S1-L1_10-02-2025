5deb98aa0fd2264c282df89290cd4dfc4e2c6327
Inchino <alessandro.incalza22@gmail.com>
2025-02-10 17:31:13 +0100

    Aggiunti file dalla directory S1-L1/

---
+++ S1-L1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace EsercizioClassiCSharp
+{
+    class Atleta
+    {
+        public string Nome { get; set; }
+        public string Sport { get; set; }
+        public int Età { get; set; }
+
+        public void MostraInfo()
+        {
+            Console.WriteLine($"Atleta: {Nome}, Sport: {Sport}, Età: {Età} anni.");
+        }
+    }
+
+    class Dipendente
+    {
+        public string Nome { get; set; }
+        public string Posizione { get; set; }
+        public double Stipendio { get; set; }
+
+        public void MostraInfo()
+        {
+            Console.WriteLine($"Dipendente: {Nome}, Posizione: {Posizione}, Stipendio: {Stipendio} euro.");
+        }
+    }
+
+    class Animale
+    {
+        public string Specie { get; set; }
+        public string Nome { get; set; }
+        public int Età { get; set; }
+
+        public void MostraInfo()
+        {
+            Console.WriteLine($"Animale: {Specie}, Nome: {Nome}, Età: {Età} anni.");
+        }
+    }
+
+    class Veicolo
+    {
+        public string Marca { get; set; }
+        public string Modello { get; set; }
+        public int Anno { get; set; }
+
+        public void MostraInfo()
+        {
+            Console.WriteLine($"Veicolo: {Marca} {Modello}, Anno: {Anno}.");
+        }
+    }
+}
+
